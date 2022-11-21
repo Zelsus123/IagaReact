62d089f6e3d3b1f207eb13c20225930adb3a9c8c
AngelTravieso <angeltraviesoc@gmail.com>
2022-11-20 22:10:16 -0400

    componente iaga

--- src/IagaApp.jsx
+++ src/IagaApp.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-import { NavBar } from "./components/NavBar";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import { AppRouter } from './router/AppRouter';
 
 
-export const App = () => {
+export const IagaApp = () => {
   return (
     <>
       
